70747cc71f5bb6607e895a053c55fc79bf481149
mokhtarmoustafa <mokhtar.rnd@gmail.com>
2022-06-29 20:22:28 +0200

    create nav graph , home fragment

--- app/src/main/java/com/mokhtar/robustatask/ui/home/MainActivity.kt
+++ app/src/main/java/com/mokhtar/robustatask/ui/home/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.mokhtar.robustatask
+package com.mokhtar.robustatask.ui.home
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.mokhtar.robustatask.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
