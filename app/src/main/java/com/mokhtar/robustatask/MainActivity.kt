8b0f0d2223cc9b7f843d59eef061cdf6b26f331b
mokhtarmoustafa <mokhtar.rnd@gmail.com>
2022-06-29 20:08:39 +0200

    Initial commit

---
+++ app/src/main/java/com/mokhtar/robustatask/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.mokhtar.robustatask
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
